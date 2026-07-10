22b7b523d732f2130a01c9b4574d0449e2cfabfd
Sc1pex <scipex.dev@gmail.com>
2026-07-10 23:19:34 +0300

    io fixes

--- include/httc/io.hpp
+++ include/httc/io.hpp
@@ -4,13 +4,14 @@
 #include <asio/buffer.hpp>
 #include <asio/ip/tcp.hpp>
 #include <expected>
+#include <functional>
 #include <string_view>
 #include <vector>
 #include "httc/server_config.hpp"
 
 namespace httc {
 
-enum class ReaderError {
+enum class ReaderError : uint8_t {
     TIMEOUT,
     CLOSED,
     UNKNOWN,
@@ -33,7 +34,7 @@ public:
 
 private:
     std::array<char, 8192> m_buffer;
-    asio::ip::tcp::socket& m_sock;
+    std::reference_wrapper<asio::ip::tcp::socket> m_sock;
 };
 
 class SocketWriter {
@@ -42,7 +43,7 @@ public:
     asio::awaitable<void> write(std::vector<asio::const_buffer> buffers);
 
 private:
-    asio::ip::tcp::socket& m_sock;
+    std::reference_wrapper<asio::ip::tcp::socket> m_sock;
 };
 
 }
