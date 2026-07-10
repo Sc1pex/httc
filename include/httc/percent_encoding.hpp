a1d81a5c6f26c1133646b6a6d5fb0c02cf37a151
Sc1pex <scipex.dev@gmail.com>
2026-07-11 01:33:45 +0300

    use string_view in percent encode/decode

--- include/httc/percent_encoding.hpp
+++ include/httc/percent_encoding.hpp
@@ -2,10 +2,11 @@
 
 #include <optional>
 #include <string>
+#include <string_view>
 
 namespace httc {
 
-std::optional<std::string> percent_decode(const std::string& str);
-std::string percent_encode(const std::string& str);
+std::optional<std::string> percent_decode(std::string_view str);
+std::string percent_encode(std::string_view str);
 
 }
--- src/percent_encoding.cpp
+++ src/percent_encoding.cpp
@@ -12,17 +12,23 @@ bool isHex(char c) {
     return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
 }
 
-std::optional<std::string> percent_decode(const std::string& str) {
+std::optional<std::string> percent_decode(std::string_view str) {
     std::string result;
 
     for (size_t i = 0; i < str.size(); i++) {
         if (str[i] == '%') {
             if (i + 2 >= str.size() || !isHex(str[i + 1]) || !isHex(str[i + 2])) {
-                return std::nullopt; // Invalid percent-encoding
+                return std::nullopt;
             }
-            std::string hex = str.substr(i + 1, 2);
-            char decoded_char = static_cast<char>(std::stoi(hex, nullptr, 16));
-            result += decoded_char;
+            auto hex = str.substr(i + 1, 2);
+            unsigned char decoded_char = 0;
+            auto [ptr, ec] = std::from_chars(hex.begin(), hex.end(), decoded_char, 16);
+
+            if (ec != std::errc{}) {
+                return std::nullopt;
+            }
+
+            result += static_cast<char>(decoded_char);
             i += 2;
         } else {
             result += str[i];
@@ -32,7 +38,7 @@ std::optional<std::string> percent_decode(const std::string& str) {
     return result;
 }
 
-std::string percent_encode(const std::string& str) {
+std::string percent_encode(std::string_view str) {
     std::string result;
 
     for (char c : str) {
