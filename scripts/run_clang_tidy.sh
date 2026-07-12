0c3d4137ed8412fc9e16eb8c3a6fa0d0cee12e98
Sc1pex <scipex.dev@gmail.com>
2026-07-12 21:51:39 +0300

    use ninja for clang tidy

--- scripts/run_clang_tidy.sh
+++ scripts/run_clang_tidy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Configure the build to export compile commands (required by clang-tidy)
-CC=clang CXX=clang++ cmake -B build/clang-tidy -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+CC=clang CXX=clang++ cmake -G Ninja -B build/clang-tidy -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 # Run clang-tidy in parallel using run-clang-tidy on all src and test files
 run-clang-tidy -p build/clang-tidy "/(src|test)/"
